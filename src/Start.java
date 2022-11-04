2988c269ccd4de3ddf4ad3c1c1701e15869f3a50
Marta Aramu <marta.aramu99@gmail.com>
2022-11-04 17:18:05 +0100

    Initial commit

---
+++ src/Start.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class Start {
+
+    public static void main(String[] args) {
+
+        String fileName = "src\\file.txt";
+
+        try (Stream<String> streams = Files.lines(Paths.get(fileName))) {
+            streams.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
